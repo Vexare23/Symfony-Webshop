ccbc75709779ea358ac3322d81b69d6295e6eae9
Suciu Andrei Cornel <horizont8@yahoo.com>
2022-01-04 13:18:24 +0200

    8 corrected + 9 + half of assigment 10

---
+++ src/Controller/ViewProducts.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ViewProducts extends AbstractController
+{
+    /**
+     * @Route("viewAllProducts", name="view_products")
+     */
+    public function __invoke(ManagerRegistry $manager): Response
+    {
+        $repository = $manager->getRepository(Product::class);
+        $product = $repository->findAll();
+        return $this->render('product/allProducts.html.twig',[
+            'products' => $product,
+        ]);
+    }
+}
